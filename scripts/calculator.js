0706aab34bc65158185259556c1a8198e86e608b
RoyDMAP <roy.dimapilis@sdgku.edu>
2024-06-17 22:19:33 -0700

    creating java script project

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -1,17 +1,33 @@
 console.log("Calculator script");
 let num1 = prompt("Enter the first number:");
 let num2 = prompt("Enter the second number:");
-let operation = prompt("Select the operation: 1. Addition, 2. substraction, 3. Multiplication, 4. Division");
+let operation = prompt("Select the operation: 1. Addition, 2. subtraction, 3. Multiplication, 4. Division");
 
 console.log(num1);
 console.log(num2);
 console.log(operation);
 
 if(operation==1){
-    //do the operation
+    //do operation
     let result = Number(num1) + Number(num2);
     //display result
     console.log(result);
 }
-
-
+if(operation==2){
+    //subtraction
+    let result = Number(num1) - Number(num2);
+    //display result
+    console.log(result);
+}
+if(operation==3){
+    //multiplication
+    let result = Number(num1) * Number(num2);
+    //display result
+    console.log(result);
+}
+if(operation==4){
+    //division
+    let result = Number(num1) / Number(num2);
+    //display result
+    console.log(result);
+}
