725110c26bf8d08e282f97850ac3d00636710b6a
RoyDMAP <roy.dimapilis@sdgku.edu>
2024-06-17 19:53:25 -0700

    creating java script

---
+++ scripts/script1.js
@@ -0,0 +1,25 @@
+
+let example = "Example";
+var example2 = "Example 2";
+
+let example3;
+example3 = 10;
+console.log(example);
+console.log(example2);
+console.log(example3);
+
+//String type variable
+let strVar = "Example";
+//Number type variable
+let nmbVar = 5;
+//Boolean type variable
+let boolVar = true;
+console.log(nmbVar+example3);
+console.log(example+example2);
+
+
+//How to get a value from a prompt
+let myAge = Number(prompt("Enter your age"));
+console.log(myAge);
+console.log(typeof(myAge));
+console.log(10 + myAge);
