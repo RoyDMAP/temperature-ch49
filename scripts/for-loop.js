1ac87689193c6c011da47c0c6be889e81308b238
RoyDMAP <roy.dimapilis@sdgku.edu>
2024-06-25 22:02:39 -0700

    creating conversion project

---
+++ scripts/for-loop.js
@@ -0,0 +1,14 @@
+console.log("Hello from for-loop.js");
+
+/*
+for (initialization; condition; increment) { code to execute when the loop runs
+}
+*/
+
+// i++ = i+=1
+
+for(let i=1; i<=10; i++) {
+    console.log("10 * " + i + "=" + 10 * i);
+}
+
+
--- scripts/temperature.js
+++ scripts/temperature.js
@@ -1,10 +1,22 @@
-function convertTemp(){
-
-    let temperature = prompt("Enter the temperature (Celsius):");
-    let f = (temperature * 9/5) + 32;
-    document.getElementById("tempResult").innerHTML = `<p> ${temperature} C to ${f} F </p>`;   
-}
+function convertTemp() {
     
+    let temperature = prompt("Choose conversion:\n1. Celsius to Fahrenheit\n2. Fahrenheit to Celsius");
+
+    if (temperature === "1") {
+        // Convert Celsius to Fahrenheit
+        let temperature = prompt("Enter the temperature in C:");
+        let f = (temperature * 9/5) + 32;
+        document.getElementById("tempResult").innerHTML = `<p>The conversion of Celsius ${temperature} to Fahrenheit is ${f}</p>`;
+    } else if (temperature === "2") {
+        // Convert Fahrenheit to Celsius
+        let temperature = prompt("Enter the temperature in Fahrenheit:");
+        let c = (temperature -32) *5/9;
+        document.getElementById("tempResult").innerHTML = `<p>The conversion of Fahrenheit ${temperature} to Celsius is ${c}</p>`;
+    } else {
+        // invalid input
+        document.getElementById("tempResult").innerHTML = "<p>Invalid selection or cancelled.</p>";
+    }
+}
 
 
 
